5a37313d2b7914a0e34189236718388fcccbe60b
Jonathan <jnthn18@gmail.com>
2016-01-21 13:09:32 -0500

    Added double click to edit location

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 var app = angular.module("weatherApp", []);
 
 app.controller('MainController', ['$scope', 'weatherFactory', function($scope, weatherFactory) {
-  $scope.location = "Ann Arbor, MI";
+  $scope.location = {name: "Ann Arbor, MI", editing: false};
   $scope.forecast = {};
+  $scope.editedLocation = null;
 
   getWeather();
 
@@ -14,6 +15,17 @@ app.controller('MainController', ['$scope', 'weatherFactory', function($scope, w
       .error(function (err) {
         console.log(err);
       });
+  }
+
+  $scope.startEditing = function(location){
+    location.editing = true;
+    $scope.editedLocation = location;
+  }
+
+  $scope.doneEditing = function(location){
+    $scope.newLocation(location);
+    location.editing = false;
+    $scope.editedLocation = null;
   }
 
   $scope.newLocation = function(location) {
@@ -28,6 +40,27 @@ app.controller('MainController', ['$scope', 'weatherFactory', function($scope, w
 
 }]);
 
+//Credit for ngBlur and ngFocus to https://github.com/addyosmani/todomvc/blob/master/architecture-examples/angularjs/js/directives/
+app.directive('ngBlur', function() {
+  return function( scope, elem, attrs ) {
+    elem.bind('blur', function() {
+      scope.$apply(attrs.ngBlur);
+    });
+  };
+});
+
+app.directive('ngFocus', function( $timeout ) {
+  return function( scope, elem, attrs ) {
+    scope.$watch(attrs.ngFocus, function( newval ) {
+      if ( newval ) {
+        $timeout(function() {
+          elem[0].focus();
+        }, 0, false);
+      }
+    });
+  };
+})
+
 app.factory('weatherFactory', ['$http', function($http) { 
 
   var baseUrl = 'http://api.wunderground.com/api/5b494ca3d635225a/forecast/q/';
@@ -38,8 +71,8 @@ app.factory('weatherFactory', ['$http', function($http) {
   };
 
   weatherFactory.updateLocation = function(location) {
-    var state = location.split(',')[1].trim();
-    var city = location.split(',')[0].replace(/ /g,"_");
+    var state = location.name.split(',')[1].trim();
+    var city = location.name.split(',')[0].replace(/ /g,"_");
     return $http.get(baseUrl+state+"/"+city+".json");
   };
 
